1ddc81b70773ff4c4f5f10fe6b8fcc81fbeea067
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2021-06-10 17:13:28 +0900

    cv hough but too big

--- scripts/cv_hough.py
+++ scripts/cv_hough.py
@@ -11,13 +11,17 @@ from cv_bridge import CvBridge
 
 def image_cb(msg):
     bridge = CvBridge()
-    img= bridge.imgmsg_to_cv2(msg, 'passthrough')
+    #img = bridge.imgmsg_to_cv2(msg, 'passthrough')
+    img = bridge.imgmsg_to_cv2(msg, 'rgb8')
     #img = cv2.imread('ellipse2.png', cv2.IMREAD_COLOR)
-
     # gray
-    gray1 = cv2.bitwise_and(img[:,:,0], img[:,:,1])
-    gray1 = cv2.bitwise_and(gray1, img[:,:,2])
-
+    #gray1 = cv2.bitwise_and(img[:,:,0], img[:,:,1])
+    #gray1 = cv2.bitwise_and(gray1, img[:,:,2])
+    #print(gray1)
+    #gray1 = 0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
+    #gray1 = np.array(gray1, dtype='int')
+    gray1 = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    cv2.imshow('resimg', gray1)
     # binary
     _, binimg = cv2.threshold(gray1, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     binimg = cv2.bitwise_not(binimg)
@@ -25,7 +29,6 @@ def image_cb(msg):
     # black to gray
     bimg = binimg // 4 + 255 * 3 //4
     resimg = cv2.merge((bimg,bimg,bimg)) 
-
     # get edge
     image,contours,hierarchy =  cv2.findContours(binimg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for i, cnt in enumerate(contours):
@@ -46,7 +49,8 @@ def image_cb(msg):
     cv2.waitKey()
 
 rospy.init_node('touch_detect')
-position_sub = rospy.Subscriber('/kinect_head/rgb/image_rect_color', Image, image_cb)
+#position_sub = rospy.Subscriber('/kinect_head/rgb/image_rect_color', Image, image_cb)
+position_sub = rospy.Subscriber('/colorize_float_image_filtered_heightmap/output', Image, image_cb)
 #pub = rospy.Publisher('r_contact', Bool, queue_size=1)
 #pub = rospy.Publisher('object_force', PoseStamped, queue_size=1)
 pub = rospy.Publisher('hough_image', Image, queue_size=1)
