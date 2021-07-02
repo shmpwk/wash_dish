aa4595c070c3c08a58fe5b6e23a96cf8d47a1f4e
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2021-07-03 01:32:41 +0900

    all force sub and save

--- scripts/sub_obj_force.py
+++ scripts/sub_obj_force.py
@@ -7,21 +7,26 @@ import rospy
 import tf
 import csv
 import pickle
+import message_filters
+import numpy as np
 from jsk_recognition_msgs.msg import *
 from geometry_msgs.msg import *
 from std_msgs.msg import *
 
-def save_data(force_x):
-    with open('x_force.pkl', 'wb') as f:
-        pickle.dump(force_x, f)
+def save_data(force_all):
+    with open('all_force.pkl', 'wb') as f:
+        pickle.dump(force_all, f)
 
 def sig_handler(signum, frame):
     sys.exit(1)
 
-def wrench_cb(msg):
-    x_force =  msg.wrench.force.x
-    y_force =  msg.wrench.force.y
-    z_force =  msg.wrench.force.z
+def wrench_cb(l_wrench, r_wrench):
+    lx_force =  l_wrench.wrench.force.x
+    ly_force =  l_wrench.wrench.force.y
+    lz_force =  l_wrench.wrench.force.z
+    rx_force =  r_wrench.wrench.force.x
+    ry_force =  r_wrench.wrench.force.y
+    rz_force =  r_wrench.wrench.force.z
     #xy = x_force**2 + y_force**2
     #yz = y_force**2 + z_force**2
     #zx = z_force**2 + x_force**2
@@ -29,27 +34,28 @@ def wrench_cb(msg):
     #print("yz", yz)
     #print("zx", zx)
     #print (y_force)
-    force_x.append(x_force)
-
+    force = np.array([[lx_force, ly_force, lz_force, rx_force, ry_force, rz_force]])
+    force_arr = np.append(force_arr, force, axis=0)
 
 def main():
     signal.signal(signal.SIGTERM, sig_handler)
     try:
         rospy.init_node('object_force')
-        #sub = rospy.Subscriber('/object_force', WrenchStamped, wrench_cb)
-        sub = rospy.Subscriber('/left_endeffector/wrench', WrenchStamped, wrench_cb)
+        #sub = rospy.Subscriber('/left_endeffector/wrench', WrenchStamped, wrench_cb)
         #pub = rospy.Publisher('r_contact', Bool, queue_size=1)
+        left_end_wrench = message_filters.Subscriber('/left_endeffector/wrench', WrenchStamped)
+        right_end_wrench = message_filters.Subscriber('/right_endeffector/wrench', WrenchStamped)
+        ts = message_filters.TimeSynchronizer([left_end_wrench, right_end_wrench], 10)
+        ts.registerCallback(wrench_cb)
         rospy.spin()
 
     finally:
         signal.signal(signal.SIGTERM, signal.SIG_IGN)
         signal.signal(signal.SIGINT, signal.SIG_IGN)
-        save_data(force_x)
+        save_data(force_arr)
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 if __name__ == "__main__":
-    force_x = []
-    force_y = []
-    force_z = []
+    force_arr = np.array([[]]).reshape(-1,6)
     sys.exit(main())
