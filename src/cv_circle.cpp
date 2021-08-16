6c4cdb52a34f39e35d3bfde84235e5a3271b0dca
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2021-08-16 10:12:17 +0900

    stil fail

--- src/cv_circle.cpp
+++ src/cv_circle.cpp
@@ -34,12 +34,19 @@ class ImageConverter{
         //cv::Mat src_img = cv::imread("./stuff.jpg", 1);
         cv::Mat src_img = cv_ptr->image;
 
-        cv::Mat gray_img, bin_img;
-        cv::cvtColor(src_img, gray_img, CV_BGR2GRAY);
+        cv::Mat gray_img, bin_img, cv_image2, hsv_image, color_mask;
+        //cv::cvtColor(src_img, gray_img, CV_BGR2GRAY);
+        cv::cvtColor(cv_ptr->image, hsv_image, CV_BGR2HSV);
+        cv::inRange(hsv_image, cv::Scalar(0, 0, 0, 0) , cv::Scalar(20, 255, 255, 0), color_mask); 
+
+        cv::bitwise_and(cv_ptr->image, cv_ptr->image, cv_image2, color_mask);
+        cv::cvtColor(cv_image2, gray_img, CV_BGR2GRAY);
+        cv::threshold(gray_img, bin_img, 0.00000000000000000001, 255, CV_THRESH_BINARY); 
+        //cv::adaptiveThreshold(gray_img, bin_img, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 5, 0);
 
         std::vector<std::vector<cv::Point> > contours;
         // binary
-        cv::threshold(gray_img, bin_img, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);
+        //cv::threshold(gray_img, bin_img, 28.9999999999999982236432, 255, cv::THRESH_BINARY);
         // get edge
         cv::findContours(bin_img, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
         jsk_recognition_msgs::RectArray rect_msg;
@@ -58,20 +65,25 @@ class ImageConverter{
         }
 
         // 最大面積を持つ輪郭の最小外接円を取得
-        cv::minEnclosingCircle(contours.at(max_area_contour), center, radius);
-        ROS_INFO("radius = %f", radius);
+        //cv::minEnclosingCircle(contours.at(max_area_contour), center, radius);
+        for(int j=0; j<contours.size(); j++){
+          cv::minEnclosingCircle(contours.at(j), center, radius);
+          ROS_INFO("radius = %f", radius);
 
-        // 最小外接円を描画
-        cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,4);
-        cv::circle(src_img, center, radius, cv::Scalar(0,0,255),3,4);
-        cv::circle(bin_img, center, radius, cv::Scalar(0,0,255),3,4);
+          // 最小外接円を描画
+          cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,4);
+          cv::circle(src_img, center, radius, cv::Scalar(0,0,255),3,4);
+          cv::circle(bin_img, center, radius, cv::Scalar(0,0,255),3,4);
+        }
 
         // 画面中心から最小外接円の中心へのベクトルを描画
-        p1 = cv::Point2f(cv_ptr->image.size().width/2,cv_ptr->image.size().height/2);
-        cv::arrowedLine(cv_ptr->image, p1, center, cv::Scalar(0, 255, 0, 0), 3, 8, 0, 0.1);  
+        //p1 = cv::Point2f(cv_ptr->image.size().width/2,cv_ptr->image.size().height/2);
+        //cv::arrowedLine(cv_ptr->image, p1, center, cv::Scalar(0, 255, 0, 0), 3, 8, 0, 0.1);  
 
         // ウインドウ表示                                                                         
         cv::imshow("Result Image", src_img);
+        cv::imshow("Gray Image", gray_img);
+        cv::imshow("Color mask Image", color_mask);
         cv::imshow("Binary Image", bin_img);
         cv::waitKey(3);
   
